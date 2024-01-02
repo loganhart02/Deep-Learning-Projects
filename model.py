4e8d81997bff1cd7220023c5a6ea02f59340917a
loganhart420 <loganhartpersonal@gmail.com>
2024-01-02 15:42:01 -0500

    add vgg16 model

--- model.py
+++ model.py
@@ -1,26 +1,75 @@
+import torch
 import torch.nn as nn
-
-
-class MyModel(nn.Module):
-    def __init__(self):
-        super(MyModel, self).__init__()
-        pass
     
+
+class Vgg16(nn.Module):
+    def __init__(self, img_channels: int = 3, num_classes: int = 1000):
+        super(Vgg16, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(img_channels, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2)
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7)) # make sure output size is 7x7 for classifier
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(4096, num_classes),
+            # nn.Softmax(dim=1) I am using crossentropy loss so it does this internally but you need to use this at inference. paper shows it so I add
+        )
+        
     def forward(self, x):
-        pass
+        x = self.avgpool(self.features(x))
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
     
-    def get_model_params(self):
-        pass
+    def get_num_params(self):
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
+        
     
     
 def predict():
     pass
 
 
-def predict_onnx():
-    pass
 
-
-def export_onnx():
-    pass
 
