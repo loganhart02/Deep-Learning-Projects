d16eb2047307e83c3187207f93966ef67cd5cb6f
loganhart420 <loganhartpersonal@gmail.com>
2024-01-03 00:24:23 -0500

    fix model weight initialization

--- model.py
+++ model.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
     
 
-class Vgg16(nn.Module):
+class Vgg19(nn.Module):
     def __init__(self, img_channels: int = 3, num_classes: int = 1000):
-        super(Vgg16, self).__init__()
+        super(Vgg19, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(img_channels, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
@@ -48,17 +48,28 @@ class Vgg16(nn.Module):
         self.classifier = nn.Sequential(
             nn.Linear(512 * 7 * 7, 4096),
             nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
+            nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
+            nn.Dropout(),
             nn.Linear(4096, num_classes),
-            # I am using crossentropy loss so it does this internally but you need to use this at inference. paper shows it so I add
-            # nn.Softmax(dim=1)
         )
         
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+        
     def forward(self, x):
-        x = self.avgpool(self.features(x))
+        x = self.features(x)
+        x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
